4f8f44e910c60dbe92b2eabef0670808d6366561
mikamo3 <kamo3proj@gmail.com>
2019-01-17 01:35:23 +0900

    :sparkles: (npm pip) add js and python's package manager

--- list.sh
+++ list.sh
@@ -3,7 +3,10 @@
 main() {
 	local target_dir
 	target_dir="$(dirname "${BASH_SOURCE[0]}")"
+  # pacman
 	pacman -Qqen | sort > "$target_dir/pacman.list"
 	pacman -Qqem | sort > "$target_dir/aur.list"
+  # npm
+  npm ls --depth=0 -g | tail -n +2 |sort| sed s'/└── //' > "$target_dir/npm.list"
 }
 main
