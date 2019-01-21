9af594fdc55331ee5605c408f5653034dfd66d6b
mikamo3 <kamo3proj@gmail.com>
2019-01-22 01:00:57 +0900

    :bug: (fix npm output)

--- list.sh
+++ list.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
 main() {
-	local target_dir
-	target_dir="$(dirname "${BASH_SOURCE[0]}")"
+  local target_dir
+  target_dir="$(dirname "${BASH_SOURCE[0]}")"
   # pacman
-	pacman -Qqen | sort > "$target_dir/pacman.list"
-	pacman -Qqem | sort > "$target_dir/aur.list"
+  pacman -Qqen | sort >"$target_dir/pacman.list"
+  pacman -Qqem | sort >"$target_dir/aur.list"
   # npm
-  npm ls --depth=0 -g | tail -n +2 |sort| sed s'/└── //' > "$target_dir/npm.list"
+  npm ls --depth=0 -g | tail -n +2 | sort | sed 's/^[├└]─*\(.*\)/\1/' > "$target_dir/npm.list"
+  # pip
+  pip freeze >"$target_dir/pip.list"
 }
 main
