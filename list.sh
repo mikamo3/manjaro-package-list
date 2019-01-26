eee6032d39cd90ea8ff10e39fb59888ff5d1f69f
mikamo3 <kamo3proj@gmail.com>
2019-01-27 00:25:52 +0900

    :sparkles: (remove base base-devel group's package)

--- list.sh
+++ list.sh
@@ -4,10 +4,10 @@ main() {
   local target_dir
   target_dir="$(dirname "${BASH_SOURCE[0]}")"
   # pacman
-  pacman -Qqen | sort >"$target_dir/pacman.list"
+  comm -23 <(pacman -Qqen | sort) <(pacman -Qgq base base-devel | sort) >"$target_dir/pacman.list"
   pacman -Qqem | sort >"$target_dir/aur.list"
   # npm
-  npm ls --depth=0 -g | tail -n +2 | sort | sed 's/^[├└]─*\(.*\)/\1/' > "$target_dir/npm.list"
+  npm ls --depth=0 -g | tail -n +2 | sort | sed 's/^[├└]─*\(.*\)/\1/' >"$target_dir/npm.list"
   # pip
   pip freeze >"$target_dir/pip.list"
 }
